d347bca9073f8153bdf62ac414b973adf3c49988
Jay <sjlkee0305@gmail.com>
2026-03-24 12:41:43 -0400

    Refactor isMatch method for regex matching
    This is Wrong I should change later

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,33 +1,49 @@
 class Solution {
-   public boolean isMatch(String s, String p) {
+        public boolean isMatch(String s, String p) {
 
-    if (s == null || p == null) {
-        return false;
-    }
-    boolean[][] dp = new boolean[s.length()+1][p.length()+1];
-    dp[0][0] = true;
-    for (int i = 0; i < p.length(); i++) {
-        if (p.charAt(i) == '*' && dp[0][i-1]) {
-            dp[0][i+1] = true;
-        }
-    }
-    for (int i = 0 ; i < s.length(); i++) {
-        for (int j = 0; j < p.length(); j++) {
-            if (p.charAt(j) == '.') {
-                dp[i+1][j+1] = dp[i][j];
+            // prevent error
+            if (s.length() != p.length()) return false;
+
+            // check p have * or .
+            char[] sl = s.toCharArray();
+            char[] pl = p.toCharArray();
+
+            int ps = 100;
+            int pd = 100;
+
+            for (int i = 0; i < s.length(); i++){
+                if(pl[i] == '*') ps = i;
+                if(pl[i] == '.') pd = i;
             }
-            if (p.charAt(j) == s.charAt(i)) {
-                dp[i+1][j+1] = dp[i][j];
+
+            // if it doesnt have it check s is equal to p
+            if (ps == 100 && pd == 100){
+                if (s.equals(p)) return true;
+                return false;
             }
-            if (p.charAt(j) == '*') {
-                if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {
-                    dp[i+1][j+1] = dp[i+1][j-1];
-                } else {
-                    dp[i+1][j+1] = (dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1]);
+
+            // if it only have . check without that position
+            if(pd != 100 && ps == 100){
+                for (int i = 0; i < s.length(); i++){
+                    if (i == pd) continue;
+                    if (sl[i] != pl[i]) return false;
                 }
             }
+            // if it only have * check front letter repeat in the back
+            char save = ' ';
+            if(pd == 100 && ps != 100){
+                for (int i = 0; i < s.length() - 1; i++){
+                    if(i == ps){
+                        save = sl[i - 1];
+                        while(i < s.length() && sl[i] == save) i++;
+                    }
+                    if (i >= s.length()) break;
+                    if (i >= p.length()) break;
+                    if (sl[i] != pl[i]) return false;
+                }
+            }
+
+            // if it have .* pass everything
+            return true;
         }
     }
-    return dp[s.length()][p.length()];
-}
-}
